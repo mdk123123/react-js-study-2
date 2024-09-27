1cfb64f11a2fadde1811c5e61e2ab5d1d3105c2c
minhkd <minhkd.ho@vietcombank.com.vn>
2024-09-27 17:13:00 +0700

    add ref and portals

---
+++ ref-portals/src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+export default function Player() {
+  const[enteredPlayerName, setEnteredPlayerName] = useState('');
+  const[submitted, setSubmitted] = useState(false);
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value) // two-way binding
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? enteredPlayerName: 'unknown entity'}</h2>
+      <p>
+        <input type="text" value = {enteredPlayerName} onChange={handleChange}/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
