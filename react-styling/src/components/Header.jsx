1fe3952af7ca8ca08a8c73e83118cc9c695ac42c
minhkd <minhkd.ho@vietcombank.com.vn>
2024-09-30 22:33:07 +0700

    change code

--- react-styling/src/components/Header.jsx
+++ react-styling/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import logo from '../assets/logo.png';
-
+import classes from './Header.module.css'
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* <p style={{
+        color: 'red',
+        textAlign: 'left'
+      }}>A community of artists and art-lovers.</p> Style in React, not in HTML, passed in as object */}
+       <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
