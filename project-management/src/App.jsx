84a275209d61b4e957f03aabe94caacb8dd1914c
minhkd <minhkd.ho@vietcombank.com.vn>
2024-10-23 23:52:23 +0700

    Add code project-management

---
+++ project-management/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjecstSidebar from "./components/ProjectsSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjecstSidebar />
+    </main>
+  );
+}
+
+export default App;
