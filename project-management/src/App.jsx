9f4ff5f6c7c8b3e1dc262ca8bf07f58838d12718
minhkd <minhkd.ho@vietcombank.com.vn>
2024-10-28 17:51:16 +0700

    Add code project management

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjecstSidebar from "./components/ProjectsSideBar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, //not add or select any project - donothing
+    projects: []
+  })
+  let content; 
+if(projectsState.selectedProjectId === null) {
+  content =<NewProject onAdd={handleAddProject}/> //lift stateup
+}
+else if(projectsState.selectedProjectId === undefined) {
+  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
+function handleStartAddProject() {
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: null, //signal to add new project
+    }
+  })
+}
+
+function handleAddProject(projectData) {
+  setProjectsState(prevState => {
+    const newProject={
+        ...projectData,
+        id: Math.random() //generate id
+    };
+
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: [...prevState.projects, newProject]
+    }
+  })
+}
+
   return (
-    <main className="h-screen my-8">
-      <ProjecstSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjecstSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
     </main>
   );
 }
---
+++ project-management/src/components/NewProject.jsx
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+
+import Input from "./Input";
+import Modal from "./Modal";
+
+export default function NewProject({onAdd}) {
+    const modal = useRef();
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate= dueDate.current.value;
+        if(enteredTitle.trim() === ''|| enteredDescription.trim() === ''|| enteredDueDate.trim() === '') {
+            //show error modal
+            modal.current.open();
+            return;
+        }
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
+    }
+
+
+
+    return <>
+        <Modal ref={modal} buttonCaption="OK">  
+            <h2>Invalid input</h2>
+            <p>Field empty</p>
+        </Modal>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                </li>
+                <li><button 
+                    className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                    onClick={handleSave}>
+                        Save</button></li>
+            </menu>
+        <div>
+           <Input type="text" label = "Title" ref={title}/>
+           <Input label = "Description" textarea  ref={description}/>
+           <Input type="date" label = "Due date" ref={dueDate}/>
+        </div>
+    </div>
+    </>
+}
