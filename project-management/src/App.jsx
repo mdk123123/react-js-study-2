43c9ec93ebf2549d81b03bcd14fa9b9748dae241
minhkd <minhkd.ho@vietcombank.com.vn>
2024-10-31 00:31:57 +0700

    add code select project

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -2,18 +2,30 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjecstSidebar from "./components/ProjectsSideBar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined, //not add or select any project - donothing
     projects: []
   })
-  let content; 
+
+  const selectedProject = projectsState.projects.find(project => project.id ===projectsState.selectedProjectId);
+  let content = <SelectedProject project={selectedProject}/>; 
 if(projectsState.selectedProjectId === null) {
-  content =<NewProject onAdd={handleAddProject}/> //lift stateup
+  content =<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/> //lift stateup
 }
 else if(projectsState.selectedProjectId === undefined) {
   content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
+
+function handleSelectProject(projectId){
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: projectId, //signal to add new project
+    }
+  })
 }
 function handleStartAddProject() {
   setProjectsState(prevState => {
@@ -39,9 +51,22 @@ function handleAddProject(projectData) {
   })
 }
 
+function handleCancelAddProject() {
+  setProjectsState((prevState) => {
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+    }
+  })
+}
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjecstSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      <ProjecstSidebar 
+      onStartAddProject={handleStartAddProject} 
+      projects={projectsState.projects}
+      onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
--- project-management/src/components/NewProject.jsx
+++ project-management/src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
     const modal = useRef();
     const title = useRef();
     const description = useRef();
@@ -28,13 +28,13 @@ export default function NewProject({onAdd}) {
 
     return <>
         <Modal ref={modal} buttonCaption="OK">  
-            <h2>Invalid input</h2>
-            <p>Field empty</p>
+            <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid input</h2>
+            <p className="text-stone-400 mb-4">Field empty</p>
         </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                    <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button>
                 </li>
                 <li><button 
                     className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
