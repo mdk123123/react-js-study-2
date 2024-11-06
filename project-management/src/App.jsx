c527b5deab1d2cf049f02e4cc22efa31e3afa481
minhkd <minhkd.ho@vietcombank.com.vn>
2024-11-06 23:26:15 +0700

    init contextAPI project

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -7,11 +7,44 @@ import SelectedProject from "./components/SelectedProject";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined, //not add or select any project - donothing
-    projects: []
+    projects: [],
+    tasks:[] //manage tasks in App components using handleClick in NewTasks
   })
 
+  function handleAddTask(text) {
+    // ...
+    setProjectsState(prevState => {
+      const taskId= Math.random();
+      const newTask={
+          text: text,
+          projectId: prevState.selectedProjectId,
+          id: taskId //generate id
+      };
+  
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks]
+      }
+    })
+  }
+
+  function handleDeleteTask(id) {
+    //...
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task)=> task.id!== id)
+      }
+    })
+  }
   const selectedProject = projectsState.projects.find(project => project.id ===projectsState.selectedProjectId);
-  let content = <SelectedProject project={selectedProject}/>; 
+  let content = <SelectedProject 
+    project={selectedProject} 
+    onDelete={handleDeleteProject} 
+    onAddTask = {handleAddTask} 
+    onDeleteTask={handleDeleteTask}
+    tasks={projectsState.tasks}
+    />; 
 if(projectsState.selectedProjectId === null) {
   content =<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/> //lift stateup
 }
@@ -24,6 +57,16 @@ function handleSelectProject(projectId){
     return {
       ...prevState,
       selectedProjectId: projectId, //signal to add new project
+    }
+  })
+}
+
+function handleDeleteProject() {
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: prevState.projects.filter((project)=> project.id!== prevState.selectedProjectId)
     }
   })
 }
@@ -66,6 +109,7 @@ function handleCancelAddProject() {
       onStartAddProject={handleStartAddProject} 
       projects={projectsState.projects}
       onSelectProject={handleSelectProject}
+      selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
--- project-management/src/components/SelectedProject.jsx
+++ project-management/src/components/SelectedProject.jsx
@@ -1,4 +1,6 @@
-export default function SelectedProject({project}) {
+import Tasks from "./Tasks";
+
+export default function SelectedProject({project, onDelete, onAddTask, onDeleteTask, tasks}) {
     //format date
     const formattedDate = new Date(project.dueDate).toLocaleDateString('vi-VN', {
         year: 'numeric',
@@ -9,11 +11,11 @@ export default function SelectedProject({project}) {
         <header className="pb-4 mb-4 border-b-2 border-stone-300">
             <div className="flex items-center justify-between">
             <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-            <button className="text-stone-600 hover:text-stone-950">Delete</button>
+            <button className="text-stone-600 hover:text-stone-950" onClick={onDelete}>Delete</button>
             </div>
             <p className="mb-4 text-stone-400 whitespace-pre-wrap">{formattedDate}</p>
             <p className="">{project.description}</p>
         </header>
-        TASKS
+        <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks}/>
     </div>);
 }
